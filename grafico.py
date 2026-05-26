c78fc28741e967aadd51e5d34a07e68cb573e4ce
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-26 15:14:26 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from matplotlib import pyplot as plt
 
 # Título
 st.title("Países Mais Seguros para Viajar")
@@ -26,3 +27,17 @@ pais = st.selectbox(
 resultado = df[df["País /Região:"] == pais]
 
 st.write(resultado)
+fig, ax = plt.subplots(figsize=(12, 6))
+df['Indícies:'] = df['Indícies:'].str.replace(',', '.', regex=False).astype(float)
+
+# Sort the DataFrame by 'Indícies:' for better visualization
+df_sorted = df.sort_values(by='Indícies:', ascending=False)
+
+# Create a bar plot
+df_sorted.plot(kind='bar', x='País /Região:', y='Indícies:', ax = ax , title='Indíces por País/Região')
+plt.xlabel('País /Região:')
+plt.ylabel('Indícies:')
+plt.xticks(rotation=45, ha='right') # Rotate x-axis labels for better readability
+plt.gca().spines[['top','right']].set_visible(False)
+plt.tight_layout() # Adjust layout to prevent labels from overlapping
+st.pyplot(fig)
