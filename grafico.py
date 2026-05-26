31b355d438134b1fa80de56b53a5f7209df80bfb
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-26 14:56:49 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-df = pd.read_csv('/content/sample_data/Paisescsv')
-
+df = pd.read_csv('
 display(df.head())
 df.to_csv('dados_exportados.csv', index=False)
 print('DataFrame exportado com sucesso para dados_exportados.csv')
