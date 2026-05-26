fba6cc79b68045878a32c0fc57b39febe8e39201
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-26 14:52:49 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.write("Olá mundo")
+import pandas as pd
+
+df = pd.read_csv('/content/sample_data/Paisescsv')
+display(df.head())
+df.to_csv('dados_exportados.csv', index=False)
+print('DataFrame exportado com sucesso para dados_exportados.csv')
